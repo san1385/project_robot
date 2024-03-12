c7cf94a57d16765e1a4d089c220cb41205184a0e
Gilbert <gilbertsanchez892@gmail.com>
2024-03-11 20:31:37 -0700

    Modified launch files

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -13,17 +13,39 @@ def generate_launch_description():
     package_name = 'project_robot'
 
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')])
-            , launch_arguments={'use_sim_time': 'true'}.items())
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
 
-    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]))
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py', arguments=['-topic', 'robot_description', '-entity', 'project_robot'], output='screen')
+
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
     # Launch!
     return LaunchDescription([
         rsp, 
         gazebo, 
         spawn_entity, 
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
